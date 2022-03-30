7ad17acd6bee7627778097cd2ca2622e6076fd84
Wikimish <majestye@hotmail.fr>
2022-03-30 08:55:06 +0200

    debut d'essai

--- test.js
+++ test.js
@@ -1,8 +1,21 @@
 let query = "dynasty"
 
-fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://catchtheshow.herokuapp.com/api/search?name=dynasty')}`)
+fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://catchtheshow.herokuapp.com/apisearch/?name=dynasty')}`)
 .then(response => {
 	if (response.ok) return response.json()
 	throw new Error('Network response was not ok.')
 })
 .then(data => console.log(data.contents));
+
+
+function afficheShows() {
+	
+	var resultat = document.createElement("div");
+	var name = document.createElement("p");
+	name.value = 
+
+
+
+	var resultats = document.getElementById("bloc-resultats");
+	document.resultats.insertBefore(resultat, null);
+}
