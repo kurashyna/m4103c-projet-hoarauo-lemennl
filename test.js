e33646c8375b54438e9a3602e6d17a1a7906c736
kurashyna <lmlucas@outlook.fr>
2022-03-30 08:12:00 +0200

    Fetch

--- test.js
+++ test.js
@@ -1,12 +1,8 @@
 let query = "dynasty"
 
-var requestHeaders = new Headers();
-var requestInit = {
-    method: 'GET',
-    headers: requestHeaders,
-    mode: 'no-cors',
-    cache: 'default'
-}
-fetch("https://catchtheshow.herokuapp.com/api/search?name=dynasty")
-    .then(response => response.text())
-    .then(res => console.log(res));
+fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://catchtheshow.herokuapp.com/api/search?name=dynasty')}`)
+.then(response => {
+	if (response.ok) return response.json()
+	throw new Error('Network response was not ok.')
+})
+.then(data => console.log(data.contents));
