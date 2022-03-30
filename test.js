aed4c1a15ea00cb4683ca5294543056f6e1ee7ee
kurashyna <lmlucas@outlook.fr>
2022-03-30 08:39:43 +0200

    data length

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
 let query = "dynasty"
+let data;
 
 fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://catchtheshow.herokuapp.com/api/search?name=dynasty')}`)
 .then(response => {
 	if (response.ok) return response.json()
 	throw new Error('Network response was not ok.')
 })
-.then(data => console.log(data.contents));
+.then((response) => {
+    data = JSON.parse(response.contents);
+    console.log(data.length);
+});
+
+const afficheData = (id) => console.log(name)   ;
