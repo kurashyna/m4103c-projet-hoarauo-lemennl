6527ec812b7bf4e37bdbaeb211a87c70bd3e7347
kurashyna <lmlucas@outlook.fr>
2022-03-30 08:49:22 +0200

    Fetch deux

--- test.js
+++ test.js
@@ -8,7 +8,16 @@ fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://catchthe
 })
 .then((response) => {
     data = JSON.parse(response.contents);
-    console.log(data.length);
+   
+    afficheData(data[0]['id']);
 });
 
-const afficheData = (id) => console.log(name)   ;
+const afficheData = (id) => fetch(`https://api.allorigins.win/get?url=${encodeURIComponent('https://catchtheshow.herokuapp.com/api'+ id)}`)
+.then(response => {
+	if (response.ok) return response.json()
+	throw new Error('Network response was not ok.')
+})
+.then ( (response) => {
+    show = JSON.parse(response.contents)
+    console.log(show);
+})
