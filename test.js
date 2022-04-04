96ec8fb02117f2fc7e14e544baab081095403ec3
Wikimish <majestye@hotmail.fr>
2022-04-04 18:03:29 +0200

    afficherInfos de fait (il faut le link avec le reste)

--- test.js
+++ test.js
@@ -30,7 +30,7 @@ function afficheShows(data){
         for ( var i = 0; i < data.length; i++ ){ //On crée une div pr chaque show et on crée les balises qui correspondent aux données
             var resultat = document.createElement("div");
     
-            var name = document.createElement("p");
+            var name = document.createElement("h3");
             name.textContent = "Titre : " + data[i]["name"];
     
             var description = document.createElement("p");
@@ -53,6 +53,8 @@ function afficheShows(data){
             resultat.appendChild(image);
             resultat.appendChild(rating);
 			resultat.appendChild(hr);
+
+			resultat.onclick = afficherInfos(data[i]["id"]);
     
             // On ajoute la div dans le bloc résultat
             resultats.appendChild(resultat);
@@ -80,6 +82,79 @@ const afficheData = (id) => fetch(`https://api.allorigins.win/get?url=${encodeUR
 const afficheDataPrediction = (elem) => fetch('https://api.allorigins.win/get?url=${encodeURIComponent(\'https://catchtheshow.herokuapp.com/api' + $(elem).attr('id'))
 
 //TODO : Afficher les shows 
+function afficherInfos(id){
+	var resultats = document.getElementById("bloc-resultats");
+        $(resultats).empty();
+
+    if (data != null) {
+
+		// Affichage des infos générales du show
+		var infosGnrl = document.createElement("div");
+
+		var creators = document.createElement("p");
+        creators.textContent = "Créateurs : " + data[i]["creators"];
+    
+        var name = document.createElement("p");
+        name.textContent = "Titre : " + data[i]["name"];
+    
+        var description = document.createElement("p");
+        description.textContent = "Description : " + data[i]["description"];
+    
+        var image = document.createElement("img");
+        image.src = "https:" + data[i]["imageUrl"];
+		
+		var status = document.createElement("p");
+		status.textContent = "Statut : " + data[i]["status"];
+
+        var rating = document.createElement("p");
+        rating.innerHTML = "Rating : " + data[i]["rating"] + "/5 " + '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">' +
+		'<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>' +
+		'</svg>';
+		
+		var hr = document.createElement("hr");
+
+		// Affichage des infos du prochain épisode du show
+		var prochainEp = document.createElement("div");
+		var intitule = document.createElement("h4");
+		intitule.textContent = "Prochain épisode :";
+
+		var nameEp = document.createElement("p");
+		nameEp.textContent = "Titre : " + data[i]["nextEpisode"]["name"];
+
+		var dateEp = document.createElement("p");
+		dateEp.textContent = "Date : " + data[i]["nextEpisode"]["day"] + "/" + data[i]["nextEpisode"]["month"] + "/" + data[i]["nextEpisode"]["year"];
+
+		var seasonEp = document.createElement("p");
+		seasonEp.textContent = "Saison : " + data[i]["nextEpisode"]["season"] + " numéro d'épisode : " + data[i]["nextEpisode"]["episode"];
+
+		
+        // On ajoute les infos principales dans la div attitrée
+        infosGnrl.appendChild(name);
+		infosGnrl.appendChild(creators);
+        infosGnrl.appendChild(description);
+        infosGnrl.appendChild(image);
+		infosGnrl.appendChild(status);
+        infosGnrl.appendChild(rating);
+		infosGnrl.appendChild(hr);
+		
+		// On ajoute les infos du prochain épisode dans la div attitrée
+		prochainEp.appendChild(nameEp);
+		prochainEp.appendChild(dateEp);
+		prochainEp.appendChild(seasonEp);
+
+        // On ajoute la div dans le bloc résultat
+        resultats.appendChild(infosGnrl);
+		resultats.appendChild(prochainEp);
+
+	} else {
+		var resultat = document.createElement("div");
+    
+        var vide = document.createElement("p");
+        vide.textContent = "Erreur. Le show est inconnu.";
+		resultat.appendChild(vide);
+		resultats.appendChild(resultat);
+	}
+}
 
 // Prédiction de recherche
 function showResults(val) {
