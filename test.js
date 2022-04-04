48298bbf1aae0a7eef4f8690efa1b797abcb0ef7
Wikimish <majestye@hotmail.fr>
2022-04-04 18:27:36 +0200

    sans erreurs de commit

--- test.js
+++ test.js
@@ -30,7 +30,12 @@ function afficheShows(data){
             var name = document.createElement("h3");
             name.textContent = "Titre : " + data[i]["name"];
     
-            var description = document.createEleme["nextEpisode"]
+            var description = document.createElement("p");
+        	description.textContent = "Description : " + data["description"];
+
+			var image = document.createElement("img");
+        	image.src = "https:" + data["imageUrl"];
+
             var rating = document.createElement("p");
             rating.innerHTML = "Rating : " + data[i]["rating"] + "/5 " + '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">' +
 			'<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>' +
